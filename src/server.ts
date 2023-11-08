8ed5c6a5510a37668f90d30322b12416217b69e2
Wen Barbosa <wenblack012@gmail.com>
2023-11-08 12:40:49 -0300

    🐞 03 Class - Created and configured EsLint 🐞

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
-const port = 3333;
+const app = fastify()
+const port = 3333
 
-app.get("/", () => {
-  return "My first Api with Fastify and Node";
-});
+app.get('/', () => {
+  return 'My first Api with Fastify and Node'
+})
 
 app.listen({ port }).then(() => {
-  console.log(`HTTP Server Running on port ${port}`);
-});
+  console.log(`HTTP Server Running on port ${port}`)
+})
