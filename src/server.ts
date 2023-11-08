d13a5ac5088596acdfc3a3905ed11e2480d4f91b
Wen Barbosa <wenblack012@gmail.com>
2023-11-08 12:04:12 -0300

    ➕ 2th Class - Created Application base (Scripts and fastify)➕

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import fastify from "fastify";
+
+const app = fastify();
+const port = 3333;
+
+app.get("/", () => {
+  return "My first Api with Fastify and Node";
+});
+
+app.listen({ port }).then(() => {
+  console.log(`HTTP Server Running on port ${port}`);
+});
