20e0432860173525b4d90ec7c9f614361b753020
Wen Barbosa <wenblack012@gmail.com>
2023-11-09 12:52:29 -0300

    🔁 Class 14 - Created GET routes (/,/:id)  🔁

--- TO-DO.md
+++ TO-DO.md
@@ -1,6 +1,6 @@
 # RF
 
-- [] O usuário deve poder criar uma nova transação;
+- [ ] O usuário deve poder criar uma nova transação;
 - [ ] O usuário deve poder obter um resumo da sua conta;
 - [ ] O usuário deve poder listar todas transações que já ocorreram;
 - [ ] O usuário deve poder visualizar uma transação única;
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,20 @@ import { z } from 'zod'
 import { randomUUID } from 'crypto'
 
 export async function transactionRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+    return { transactions }
+  })
+
+  app.get('/:id', async (req, res) => {
+    const transactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = transactionParamsSchema.parse(req.params)
+    const transaction = await knex('transactions').where('id', id).first()
+    return { transaction }
+  })
+
   app.post('/', async (req, res) => {
     const transactionBodySchema = z.object({
       title: z.string(),
