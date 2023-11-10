037a4ba3030d547fd27a61baddd839dee3d156c7
Wen Barbosa <wenblack012@gmail.com>
2023-11-10 15:15:16 -0300

    fix: Alterd build configs of project

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { env } from './env'
 import { app } from './app'
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log(`HTTP Server Running on port ${env.PORT}`)
-})
+app
+  .listen({
+    port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+  })
+  .then(() => {
+    console.log(`HTTP Server Running on port ${env.PORT}`)
+  })
