39e6952834180c41e9db4de04718445919a89fad
Wen Barbosa <wenblack012@gmail.com>
2023-11-10 15:01:51 -0300

    chore: Prepare project to build

--- src/env/index.ts
+++ src/env/index.ts
@@ -9,8 +9,9 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['pg', 'sqlite']).default('sqlite'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
